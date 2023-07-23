c97433d8940a2427ecba29bc6691021443be7e0e
YOUSSEFNAHI2 <inscrire123@gmail.com>
2023-07-23 15:40:08 +0100

    m

--- myenglish/data.js
+++ myenglish/data.js
@@ -1,5 +1,6 @@
 words = [
-    'abroad','access','test','gggg'
+    {w:'abroad',t:"adv"},
+    {w:'access',t:"n,v"},
 ];
 
 data = [
@@ -9,8 +10,8 @@ data = [
             def:"in or to a different country",
             ar:"في الخارج",
             ex:[
-                "Are you going <strong>abroad</strong> this summer?",
-                "I would like to study <strong>abroad</strong> next year."
+                "Are you going abroad this summer?",
+                "I would like to study abroad next year."
             ]
         },
     ],
@@ -20,8 +21,24 @@ data = [
             def:"the fact of being able to use or see something",
             ar:"وُصول",
             ex:[
-                "Are you going <strong>abroad</strong> this summer?",
-                "I would like to study <strong>abroad</strong> next year."
+                "Do you have access to a computer?",
+                "internet access"
+            ]
+        },
+        {
+            word:"access",
+            def:"the way that you reach or go into a place",
+            ar:"دُخول",
+            ex:[
+                "The only access to the island is by boat.",
+            ]
+        },
+        {
+            word:"access",
+            def:"to be able to find and see information, especially using a computer",
+            ar:"يَحصُل (عَلَى مَعلومات مِن جِهاز الحاسوب)",
+            ex:[
+                "You will need a password to access those files.",
             ]
         },
     ]
