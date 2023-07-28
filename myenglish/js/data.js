784d2e0c611366ed4616fa2d7b193c5cf7de1ffc
YOUSSEFNAHI2 <inscrire123@gmail.com>
2023-07-28 22:46:46 +0100

    m

--- myenglish/js/data.js
+++ myenglish/js/data.js
@@ -1,45 +1,45 @@
 words = [
-    {w:'abroad',t:"adv"},
-    {w:'access',t:"n,v"},
+    {w:'abroad',t:"adv",link:"https://dictionary.cambridge.org/dictionary/english-arabic/abroad"},
+    {w:'access',t:"n,v",link:"https://dictionary.cambridge.org/dictionary/english-arabic/access"},
 ];
 
-data = [
-    [
-        {
-            word:"abroad",
-            def:"in or to a different country",
-            ar:"في الخارج",
-            ex:[
-                "Are you going abroad this summer?",
-                "I would like to study abroad next year."
-            ]
-        },
-    ],
-    [
-        {
-            word:"access",
-            def:"the fact of being able to use or see something",
-            ar:"وُصول",
-            ex:[
-                "Do you have access to a computer?",
-                "internet access"
-            ]
-        },
-        {
-            word:"access",
-            def:"the way that you reach or go into a place",
-            ar:"دُخول",
-            ex:[
-                "The only access to the island is by boat.",
-            ]
-        },
-        {
-            word:"access",
-            def:"to be able to find and see information, especially using a computer",
-            ar:"يَحصُل (عَلَى مَعلومات مِن جِهاز الحاسوب)",
-            ex:[
-                "You will need a password to access those files.",
-            ]
-        },
-    ]
-];
+// data = [
+//     [
+//         {
+//             word:"abroad",
+//             def:"in or to a different country",
+//             ar:"في الخارج",
+//             ex:[
+//                 "Are you going abroad this summer?",
+//                 "I would like to study abroad next year."
+//             ]
+//         },
+//     ],
+//     [
+//         {
+//             word:"access",
+//             def:"the fact of being able to use or see something",
+//             ar:"وُصول",
+//             ex:[
+//                 "Do you have access to a computer?",
+//                 "internet access"
+//             ]
+//         },
+//         {
+//             word:"access",
+//             def:"the way that you reach or go into a place",
+//             ar:"دُخول",
+//             ex:[
+//                 "The only access to the island is by boat.",
+//             ]
+//         },
+//         {
+//             word:"access",
+//             def:"to be able to find and see information, especially using a computer",
+//             ar:"يَحصُل (عَلَى مَعلومات مِن جِهاز الحاسوب)",
+//             ex:[
+//                 "You will need a password to access those files.",
+//             ]
+//         },
+//     ]
+// ];
--- myenglish/js/script.js
+++ myenglish/js/script.js
@@ -2,25 +2,25 @@ words.forEach((element,index) => {
     // alert(index)
     modulo = index%2;
     if (modulo==0) {
-        $("#table").append(`<tr id=${index}><td id=${element}-word><a href="#${element}-def"><strong>${element.w}</strong><small>(${element.t})</small></a></td></tr>`)
+        $("#table").append(`<tr id=${index}><td id=${element.w}-word><a href="${element.link}"><strong>${element.w}</strong><small>(${element.t})</small></a></td></tr>`)
     }
     else {
-        $("#"+(index-1)).append(`<td id=${element}-word><a href="#${element}-def"><strong>${element.w}</strong><small>(${element.t})</small></a></td>`)
+        $("#"+(index-1)).append(`<td id=${element.w}-word><a href="${element.link}"><strong>${element.w}</strong><small>(${element.t})</small></a></td>`)
     }
     
 });
 
 
-data.forEach((el,index) => {
-    // alert(index)
-    $("#definition").append(`<div class="divider">-----------------<strong>${words[index].w}</strong>------------------</div>`)
-    $("#definition").append(`<div class="w_def" id="w_def${index}"></div>`)
-    el.forEach(element => {
-        console.log(element)
-        $("#w_def"+index).append(`<div class="def" id="${element.word}-def"><strong>${element.def}<span class="ar">${element.ar}</span></strong> <a class="b" href="#${element.word}-word">Back</a></div>`)
-        element.ex.forEach(e => {
-            e= e.replace(element.word, '<strong>'+element.word+'</strong>')
-            $("#w_def"+index).append(`<div class="def-ex"><span class="ex">${e}</span></div>`)
-        })
-    })
-});
+// data.forEach((el,index) => {
+//     // alert(index)
+//     $("#definition").append(`<div class="divider">-----------------<strong>${words[index].w}</strong>------------------</div>`)
+//     $("#definition").append(`<div class="w_def" id="w_def${index}"></div>`)
+//     el.forEach(element => {
+//         console.log(element)
+//         $("#w_def"+index).append(`<div class="def" id="${element.word}-def"><strong>${element.def}<span class="ar">${element.ar}</span></strong> <a class="b" href="#${element.word}-word">Back</a></div>`)
+//         element.ex.forEach(e => {
+//             e= e.replace(element.word, '<strong>'+element.word+'</strong>')
+//             $("#w_def"+index).append(`<div class="def-ex"><span class="ex">${e}</span></div>`)
+//         })
+//     })
+// });
